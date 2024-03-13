fccfc2496ed17dc9637a42c2aa01bade5bfd2255
brunoosuna8 <brunoosuna8@gmail.com>
2024-03-13 19:47:06 -0300

    add:yt video player superposition, script of listener events

--- js/video.js
+++ js/video.js
@@ -1,9 +1,39 @@
-const video = document.getElementById("video");
+let player;
+let video;
+let closeButton = document.getElementById("player-close-button");
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "390",
+    width: "640",
+    videoId: "Q2z7rnIWSQ0",
+    playerVars: {
+      playersinline: 1,
+      autoplay: 0,
+      controls: 1,
+    },
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+  });
+}
 
-video.addEventListener("play", function () {
-  video.id.add("video-fullscreen");
-});
-
-video.addEventListener("pause", function () {
+function onPlayerReady() {
+  console.log("ready");
+  video = document.getElementById("player");
+  console.log(video);
+}
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    done = true;
+    video.classList.add("video-fullscreen");
+    closeButton.style.display = "block";
+    closeButton.classList.add("player-close-button-style");
+  }
+  console.log(done);
+}
+closeButton.addEventListener("click", function () {
   video.classList.remove("video-fullscreen");
+  closeButton.style.display = "none";
 });
