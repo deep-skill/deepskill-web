e17541c26cebb841dd9accdb160c756b1ee669c1
brunoosuna8 <brunoosuna8@gmail.com>
2024-03-14 15:52:20 -0300

    fix: video aspect ratio 16:9 convertion with js

--- js/video.js
+++ js/video.js
@@ -1,11 +1,12 @@
 let player;
 let video;
 let closeButton = document.getElementById("player-close-button");
+let isVideoFullScreen = false;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
-    height: "390",
-    width: "640",
     videoId: "Q2z7rnIWSQ0",
+    // width: 560,
+    // height: 315,
     playerVars: {
       playersinline: 1,
       autoplay: 0,
@@ -19,9 +20,7 @@ function onYouTubeIframeAPIReady() {
 }
 
 function onPlayerReady() {
-  console.log("ready");
   video = document.getElementById("player");
-  console.log(video);
 }
 var done = false;
 function onPlayerStateChange(event) {
@@ -30,10 +29,70 @@ function onPlayerStateChange(event) {
     video.classList.add("video-fullscreen");
     closeButton.style.display = "block";
     closeButton.classList.add("player-close-button-style");
+    isVideoFullScreen = true;
+    makeFullScreen();
+  } else {
+    done = false;
   }
   console.log(done);
 }
 closeButton.addEventListener("click", function () {
   video.classList.remove("video-fullscreen");
   closeButton.style.display = "none";
+  isVideoFullScreen = false;
+  video.style.width = "320px";
+  video.style.height = "180px";
+  // video.style.width = "320px";
+  // video.style.height = "180px";
 });
+
+function makeFullScreen() {
+  var playerOriginalWidth = 16;
+  var playerOriginalHeight = 9;
+  var windowHeight = window.innerHeight;
+  var windowWidth = window.innerWidth;
+
+  var playerHeight = windowHeight * 0.7;
+  var playerWidth = windowWidth * 0.7;
+  if (
+    (playerHeight * playerOriginalWidth) / playerOriginalHeight >
+    windowWidth * 0.7
+  ) {
+    playerHeight =
+      (windowWidth * 0.7 * playerOriginalHeight) / playerOriginalWidth;
+  } else {
+    playerWidth =
+      (windowHeight * 0.7 * playerOriginalWidth) / playerOriginalHeight;
+  }
+
+  video.style.width = playerWidth + "px";
+  video.style.height = playerHeight + "px";
+}
+window.onresize = function (event) {
+  if (isVideoFullScreen) makeFullScreen();
+  else resizeVideo();
+};
+
+function resizeVideo() {
+  var playerOriginalWidth = 16;
+  var playerOriginalHeight = 9;
+  var windowHeight = window.innerHeight;
+  var windowWidth = window.innerWidth;
+
+  var playerHeight = windowHeight * 0.2;
+  var playerWidth = windowWidth * 0.2;
+
+  if (
+    (playerHeight * playerOriginalWidth) / playerOriginalHeight >
+    windowWidth * 0.2
+  ) {
+    playerHeight =
+      (windowWidth * 0.2 * playerOriginalHeight) / playerOriginalWidth;
+  } else {
+    playerWidth =
+      (windowHeight * 0.2 * playerOriginalWidth) / playerOriginalHeight;
+  }
+
+  video.style.width = playerWidth + "px";
+  video.style.height = playerHeight + "px";
+}
