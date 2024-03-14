c74c913165c9c96ecc5c5afd10712fb40a665f5a
brunoosuna8 <brunoosuna8@gmail.com>
2024-03-14 17:33:33 -0300

    fix: optimize video.js, responsive video resize

--- js/video.js
+++ js/video.js
@@ -2,11 +2,10 @@ let player;
 let video;
 let closeButton = document.getElementById("player-close-button");
 let isVideoFullScreen = false;
+
 function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
     videoId: "Q2z7rnIWSQ0",
-    // width: 560,
-    // height: 315,
     playerVars: {
       playersinline: 1,
       autoplay: 0,
@@ -30,7 +29,7 @@ function onPlayerStateChange(event) {
     closeButton.style.display = "block";
     closeButton.classList.add("player-close-button-style");
     isVideoFullScreen = true;
-    makeFullScreen();
+    resizeVideo();
   } else {
     done = false;
   }
@@ -40,57 +39,39 @@ closeButton.addEventListener("click", function () {
   video.classList.remove("video-fullscreen");
   closeButton.style.display = "none";
   isVideoFullScreen = false;
-  video.style.width = "320px";
-  video.style.height = "180px";
-  // video.style.width = "320px";
-  // video.style.height = "180px";
+  resizeVideo();
 });
-
-function makeFullScreen() {
-  var playerOriginalWidth = 16;
-  var playerOriginalHeight = 9;
-  var windowHeight = window.innerHeight;
-  var windowWidth = window.innerWidth;
-
-  var playerHeight = windowHeight * 0.7;
-  var playerWidth = windowWidth * 0.7;
-  if (
-    (playerHeight * playerOriginalWidth) / playerOriginalHeight >
-    windowWidth * 0.7
-  ) {
-    playerHeight =
-      (windowWidth * 0.7 * playerOriginalHeight) / playerOriginalWidth;
-  } else {
-    playerWidth =
-      (windowHeight * 0.7 * playerOriginalWidth) / playerOriginalHeight;
-  }
-
-  video.style.width = playerWidth + "px";
-  video.style.height = playerHeight + "px";
-}
 window.onresize = function (event) {
-  if (isVideoFullScreen) makeFullScreen();
-  else resizeVideo();
+  resizeVideo();
 };
 
 function resizeVideo() {
+  let screenPorcentageOcupped = isVideoFullScreen ? 0.7 : 0.2;
+  if (!isVideoFullScreen && window.innerWidth <= 1024)
+    screenPorcentageOcupped = 0.45;
+  if (!isVideoFullScreen && window.innerWidth <= 768)
+    screenPorcentageOcupped = 0.5;
+  if (!isVideoFullScreen && window.innerWidth <= 580)
+    screenPorcentageOcupped = 0.7;
   var playerOriginalWidth = 16;
   var playerOriginalHeight = 9;
   var windowHeight = window.innerHeight;
   var windowWidth = window.innerWidth;
 
-  var playerHeight = windowHeight * 0.2;
-  var playerWidth = windowWidth * 0.2;
+  var playerHeight = windowHeight * screenPorcentageOcupped;
+  var playerWidth = windowWidth * screenPorcentageOcupped;
 
   if (
     (playerHeight * playerOriginalWidth) / playerOriginalHeight >
-    windowWidth * 0.2
+    windowWidth * screenPorcentageOcupped
   ) {
     playerHeight =
-      (windowWidth * 0.2 * playerOriginalHeight) / playerOriginalWidth;
+      (windowWidth * screenPorcentageOcupped * playerOriginalHeight) /
+      playerOriginalWidth;
   } else {
     playerWidth =
-      (windowHeight * 0.2 * playerOriginalWidth) / playerOriginalHeight;
+      (windowHeight * screenPorcentageOcupped * playerOriginalWidth) /
+      playerOriginalHeight;
   }
 
   video.style.width = playerWidth + "px";
